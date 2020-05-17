ae9e6692840f641f49d2a6d7ac5d07a56f3f08b9
JerryWang <Jerry.Wang2@walmart.com>
2020-05-17 18:50:02 +0800

    temp add

---
+++ 3hnst/hn_app/lib/src/util/json_parse.dart
@@ -0,0 +1,9 @@
+import 'package:hnapp/src/model/article.dart';
+
+List<int> parseTopStoryList(String json) {
+  throw UnimplementedError();
+}
+
+Article parseArticle(String json) {
+  throw UnimplementedError();
+}
